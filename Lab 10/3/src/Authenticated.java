d7e152328f4211106a25d248ccb44fa712c90b0a
Krishna Sri Somepalli <krishnasri.somepalli@outlook.com>
2018-03-26 19:13:33 +0530

    Lab 10

---
+++ Lab 10/3/src/Authenticated.java
@@ -0,0 +1,18 @@
+import java.io.*; 
+import javax.servlet.*; 
+import javax.servlet.http.*; 
+ 
+public class Authenticated extends HttpServlet { 
+ 
+ public void doPost(HttpServletRequest request, 
+ HttpServletResponse response) 
+ throws ServletException, IOException 
+ { 
+    response.setContentType("text/html"); 
+    PrintWriter pwriter = response.getWriter(); 
+ 
+    String name=request.getParameter("username"); 
+    pwriter.print("Hello "+name+"!");
+ } 
+ 
+} 
---
+++ Lab 10/3/src/Login.java
@@ -0,0 +1,28 @@
+import java.io.*;  
+import java.io.*;  
+import javax.servlet.*;  
+import javax.servlet.http.*;      
+public class Login extends HttpServlet 
+{    
+   public void doPost(HttpServletRequest request, 
+     HttpServletResponse response) 
+       throws ServletException, IOException 
+   {        
+       response.setContentType("text/html");      
+       PrintWriter pwriter = response.getWriter();                
+       String name = request.getParameter("username");      
+       String pass = request.getParameter("password");                
+       if(name.equals("Jane") && 
+          pass.equals("Doe"))
+       {          
+          RequestDispatcher dis = request.getRequestDispatcher("authenticated");          
+          dis.forward(request, response);      
+       }     
+       else
+       {      
+          pwriter.print("Username or password is incorrect!\n<br/><br/>");          
+          RequestDispatcher dis = request.getRequestDispatcher("index.html");          
+          dis.include(request, response);                                
+       }      
+   }    
+}  
